ae4b251166713526475c49c530e659bf796ad51e
Anton Anikonov <antonanikonov1@gmail.com>
2023-08-31 21:57:13 +0300

    add images for weather

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 const apiKey  = "2267599290a98fc36801348ab146490d";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const searchBox = document.querySelector(".search input")
-const searchBtn = document.querySelector(".search button")
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector('.weather-icon')
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -14,7 +15,17 @@ async function checkWeather(city){
     document.querySelector('.humidity').innerHTML = data.main.humidity +"%";
     document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
 
-
+    if (data.weather[0].main =="Clouds"){
+        weatherIcon.src = "img/clouds.png";
+    }else if(data.weather[0].main =="Clear"){
+        weatherIcon.src = "img/clear.png";
+    }else if(data.weather[0].main =="Rain"){
+        weatherIcon.src = "img/rain.png";
+    }else if(data.weather[0].main =="Drizzle"){
+        weatherIcon.src = "img/drizzle.png";
+    }else if(data.weather[0].main =="Mist"){
+        weatherIcon.src = "img/mist.png";
+    }
 
 }
 searchBtn.addEventListener('click',()=>{
